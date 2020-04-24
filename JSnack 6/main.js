8fdb3f9da4224bb3ae736105adc9293c16895da2
Paolo <paolo.suero@gmail.com>
2020-04-24 17:16:14 +0200

    Perfezionato esercizio

--- JSnack 6/main.js
+++ JSnack 6/main.js
@@ -8,6 +8,7 @@ var potenza = 0;
 
 //stampo le potenze di 2 fino a quando il risultato è pari a 1000 (parto dall'esponente pari ad uno)
 for (var i = 0; potenza <= 1000 ; i++) {
-    var potenza = Math.pow(base_potenza, i);
     console.log(potenza);
+    var potenza = Math.pow(base_potenza, i);
+
 }
