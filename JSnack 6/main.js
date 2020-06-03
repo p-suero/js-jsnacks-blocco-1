89fd1d58f1ccf1a2918bbea0e19d5de87b03dc9d
Paolo <paolo.suero@gmail.com>
2020-06-03 15:36:33 +0200

    Modificato commento

--- JSnack 6/main.js
+++ JSnack 6/main.js
@@ -6,7 +6,7 @@ var base_potenza = 2;
 //creo una variabile per avvirare il ciclo
 var potenza = 0;
 
-//stampo le potenze di 2 fino a quando il risultato è pari a 1000 (parto dall'esponente pari ad uno)
+//stampo le potenze di 2 fino a quando il risultato è pari a 1000
 for (var i = 0; potenza <= 1000 ; i++) {
     console.log(potenza);
     var potenza = Math.pow(base_potenza, i);
